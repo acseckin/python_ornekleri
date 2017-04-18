034a08891c6a1c88ea94ae2db0ab1942397cbdcc
omergaffar <omergaffar0064@gmail.com>
2017-04-18 14:16:58 +0300

    Update d005_1_denklem_cozme.py

--- d005_1_denklem_cozme.py
+++ d005_1_denklem_cozme.py
@@ -5,16 +5,21 @@ Created on Sun Mar 19 16:15:23 2017
 
 @author: acs
 """
-#dfsdf
+
 from sympy.solvers import solve
+#sympy komutuyla solvers komutunu çalıştırıyoruz.
 from sympy import Symbol
 import numpy as np
 from matplotlib import pyplot as plt
-
+#import yaparak matematiksel işlemleri yapıyoruz.
+#koordinat düzlemimde x ve y'yi tanımlıyoruz
 x = Symbol('x')
+#symbol komutunu x'e tanımlıyoruz.
 y = np.arange(-1.0,1.01,0.1)
+#arange komutuyla koordinatları giriyoruz
 xp=[]
 xn=[]
+#for döngüsünü kullanarak dairenin matematiksel komutlarını giriyoruz
 for yi in y:
     sol=solve(x**2 +yi**2-1.0,x)
     print sol
